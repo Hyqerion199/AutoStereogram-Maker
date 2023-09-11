e9f8089b12465e07c7930f1439049fab9d93cb28
Hyqerion199 <matjel08@gmail.com>
2023-09-11 16:37:10 -0400

    there

--- merge.py
+++ merge.py
@@ -9,15 +9,19 @@ def get_concat_v(im1, im2):
 	dst.paste(im2, (0, im1.height))
 	return dst
 
-for file in glob.glob("./rgb/*.jpg"):
+"""for file in glob.glob("./rgb/*.jpg"):
 	im1 = Image.open(file)
 	try:
 		im2 = Image.open(file.replace('rgb', 'averaged'))
 	except:
 		im2 = Image.open(file.replace('rgb', 'depth'))
-	
 	get_concat_v(im1, im2).save(file.replace('rgb', 'merged'))
 	im2 = Image.open(file.replace('rgb', 'depth'))
 	get_concat_v(im1, im2).save(file.replace('rgb', 'merged2'))
+	print("Merged: " + file)"""
+for file in glob.glob("./depth/*.jpg"):
+	im1 = Image.open(file)
+	im2 = Image.open(file.replace('depth', 'averaged'))
+	get_concat_v(im1, im2).save(file.replace('depth', 'merged3'))
 	print("Merged: " + file)
 print('Done.')
