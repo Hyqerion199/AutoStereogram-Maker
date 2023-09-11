50d3383ca3e68b9dfb23bbb6abb9001ac40be94e
Hyqerion199 <matjel08@gmail.com>
2023-09-11 18:04:55 -0400

    i need to make my own stereogram maker

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import matplotlib.image as mpimg
 import numpy as np
 
 
-source_image = mpimg.imread("./depth/000024.jpg")
+source_image = mpimg.imread("./depth_map.jpg")
 
 # Use numpy to randomly generate some noise
 image_data = np.array(source_image * 255, dtype=int)
-
 converter = StereogramConverter()
-print(image_data)
 result = converter.convert_depth_to_stereogram(image_data).astype(np.uint8)
